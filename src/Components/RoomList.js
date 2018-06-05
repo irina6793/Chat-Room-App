54951d3817f2bbde9df13c7fea9051c338a83ea9
Irina Sverzhanovskaya <irina6793@yahoo.com>
2018-06-04 21:50:21 -0700

    new

--- src/Components/RoomList.js
+++ src/Components/RoomList.js
@@ -7,7 +7,17 @@ class RoomList extends Component {
     this.state = {
       rooms: []
     };
+    this.roomsRef = this.props.firebase.database().ref('rooms');
   }
+
+  componentDidMount() {
+       this.roomsRef.on('child_added', snapshot => {
+         const room = snapshot.val();
+         room.key = snapshot.key;
+         this.setState({ rooms: this.state.rooms.concat( room ) })
+       });
+     }
+
    render() {
      return (
        <div className="RoomList">
