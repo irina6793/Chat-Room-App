13840e69c32fcbb056db953fba3d45e5e4f309f1
Irina <irina6793@yahoo.com>
2019-06-09 14:51:06 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,62 +1,75 @@
-import React, { Component } from 'react';
-import './App.css';
-import * as firebase from 'firebase';
-import RoomList from './components/RoomList';
-import MessageList from './components/MessageList';
-import User from './components/User';
+import React, { Component } from "react";
+import "./App.css";
+import * as firebase from "firebase";
+import RoomList from "./components/RoomList";
+import MessageList from "./components/MessageList";
+import User from "./components/User";
 
 var config = {
-    apiKey: "AIzaSyAznF2GHVzfH2N1-mf_-wB1JJuO1e3H8QY",
-    authDomain: "chat-room-a977e.firebaseapp.com",
-    databaseURL: "https://chat-room-a977e.firebaseio.com",
-    projectId: "chat-room-a977e",
-    storageBucket: "chat-room-a977e.appspot.com",
-    messagingSenderId: "99045457457"
-  };
-  firebase.initializeApp(config);
+  apiKey: "AIzaSyAznF2GHVzfH2N1-mf_-wB1JJuO1e3H8QY",
+  authDomain: "chat-room-a977e.firebaseapp.com",
+  databaseURL: "https://chat-room-a977e.firebaseio.com",
+  projectId: "chat-room-a977e",
+  storageBucket: "chat-room-a977e.appspot.com",
+  messagingSenderId: "99045457457"
+};
+firebase.initializeApp(config);
 
 class App extends Component {
   constructor(props) {
-    super(props)
-    this.state ={
+    super(props);
+    this.state = {
       message: [],
       roomId: [],
-      activeRoom: '',
-      user: ''
-
-    }
+      activeRoom: "",
+      user: ""
+    };
     this.setRoom = this.setRoom.bind(this);
     this.setUser = this.setUser.bind(this);
   }
 
-  setRoom(room){
-      this.setState({activeRoom: room})
-      console.log(this.state.activeRoom.key)
+  setRoom(room) {
+    this.setState({ activeRoom: room });
+    console.log(this.state.activeRoom.key);
   }
 
-  setUser(user){
+  setUser(user) {
     console.log("setUser was called.");
-    this.setState({ user : user})
+    this.setState({ user: user });
     if (user) {
-       console.log("set user to " + user.displayName);
-     } else {
-       console.log("null user was provided.");
-     }
-}
+      console.log("set user to " + user.displayName);
+    } else {
+      console.log("null user was provided.");
+    }
+  }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">The Chat Room</h1>
-          <h1 className="Room-List">Welcome to Chat Rooms</h1>
+          <h1 className="Room-List">Welcome to The Chat Room</h1>
         </header>
-        <RoomList firebase={firebase} activeRoom={this.state.activeRoom} setRoom={this.setRoom} />
-        <MessageList firebase={firebase} activeRoom={this.state.activeRoom} user={this.state.user} setUser={this.setUser} />
-        <User firebase={firebase} activeRoom={this.state.activeRoom} setUser={this.setUser} user={this.state.user} />
-         </div>
-     );
+        <RoomList
+          firebase={firebase}
+          activeRoom={this.state.activeRoom}
+          setRoom={this.setRoom}
+        />
+        <MessageList
+          firebase={firebase}
+          activeRoom={this.state.activeRoom}
+          user={this.state.user}
+          setUser={this.setUser}
+        />
+        <User
+          firebase={firebase}
+          activeRoom={this.state.activeRoom}
+          setUser={this.setUser}
+          user={this.state.user}
+        />
+      </div>
+    );
   }
 }
 
