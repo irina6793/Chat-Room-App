40c3eb5a0c28b08d51db0b76f18caf0e0242e937
Irina <irina6793@yahoo.com>
2019-06-23 18:21:02 -0400

    update

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,70 +1,66 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class RoomList extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
       rooms: [],
-      newRoomsName: '',
-      activeRoom: ''
-    }
+      newRoomsName: "",
+      activeRoom: ""
+    };
     this.handleChange = this.handleChange.bind(this);
-    this.roomsRef = this.props.firebase.database().ref('rooms');
-   }
-
-   handleChange(e) {
-     e.preventDefault();
-     this.setState({
-       newRoomsName: e.target.value
-     });
-   }
-
-   handleSubmit(e) {
-     e.preventDefault();
-     if (this.state.newRoomsName)
-     {
-       this.setState({newRoomsName: ''});
-       this.roomsRef.push({
-       name: this.state.newRoomsName,
-       })
-     }
-}
-
-  componentDidMount() {
-       this.roomsRef.on('child_added', snapshot => {
-       const rooms = snapshot.val();
-       rooms.key = snapshot.key;
-       this.setState({ rooms: this.state.rooms.concat( rooms ) });
-    });
-
+    this.roomsRef = this.props.firebase.database().ref("rooms");
   }
 
-   render() {
-     return (
-       <div className = 'rooms'>
-       <input type="text" value={this.state.newRoomsName} onChange={this.handleChange.bind(this)} />
-       <form onSubmit= { (e) => this.handleSubmit(e) }>
-       <button type="submit">Submit</button>
-       <label>
-         Room: {this.props.activeRoom.name}
-        </label>
-         <span> {this.state.description} </span>
-       </form>
-       <ul>
-         {
-           this.state.rooms.map((room, i) => {
-          return (
-            <li key={i} onClick={() => this.props.setRoom(room)} >
-              {room.name}
-           </li>
+  handleChange(e) {
+    e.preventDefault();
+    this.setState({
+      newRoomsName: e.target.value
+    });
+  }
 
-          )
-     })
-   }
-         </ul>
+  handleSubmit(e) {
+    e.preventDefault();
+    if (this.state.newRoomsName) {
+      this.setState({ newRoomsName: "" });
+      this.roomsRef.push({
+        name: this.state.newRoomsName
+      });
+    }
+  }
+
+  componentDidMount() {
+    this.roomsRef.on("child_added", snapshot => {
+      const rooms = snapshot.val();
+      rooms.key = snapshot.key;
+      this.setState({ rooms: this.state.rooms.concat(rooms) });
+    });
+  }
+
+  render() {
+    return (
+      <div className="rooms">
+        <input
+          type="text"
+          value={this.state.newRoomsName}
+          onChange={this.handleChange.bind(this)}
+        />
+        <form onSubmit={e => this.handleSubmit(e)}>
+          <button type="submit">Submit</button>
+          <label>Room: {this.props.activeRoom.name}</label>
+          <span> {this.state.description} </span>
+        </form>
+        <ul>
+          {this.state.rooms.map((room, i) => {
+            return (
+              <li key={i} onClick={() => this.props.setRoom(room)}>
+                {room.name}
+              </li>
+            );
+          })}
+        </ul>
       </div>
     );
-             }
-           }
-   export default RoomList;
+  }
+}
+export default RoomList;
